20777f40b6aafe4777c68c6b5369cef9dd33c6a1
thapaRoyal <thaparoyal17@gmail.com>
2022-04-08 22:26:07 +0545

    user registration controller part1

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,24 @@ const app = express();
 // DB
 DbConnect();
 
+// Register route (post request))
+app.post('/api/users/register', (req, res) => {
+  // business logic
+  res.json({ user: 'User registered' });
+});
+
+// Login route (post request))
+app.post('/api/users/login', (req, res) => {
+  // business logic
+  res.json({ user: 'Login successful' });
+});
+
+// fetch all users route (get request))
+app.get('/api/users', (req, res) => {
+  // business logic
+  res.json({ user: 'All users fetched' });
+});
+
 // PORT
 const PORT = process.env.PORT || 8000;
 

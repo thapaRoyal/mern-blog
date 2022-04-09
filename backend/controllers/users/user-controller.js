584492cf661959f5c8cbd402e4a4b86349febcf8
thapaRoyal <thaparoyal17@gmail.com>
2022-04-09 12:34:40 +0545

    check if user  already exists

--- backend/controllers/users/user-controller.js
+++ backend/controllers/users/user-controller.js
@@ -5,6 +5,16 @@ const User = require('../../models/user/User-model');
 const userRegisterController = async (req, res) => {
   // destructureRequestBody(req);
   const { firstName, lastName, email, password } = req.body;
+
+  // Check if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res.status(400).json({
+      error: 'User already exists',
+    });
+  }
+
   try {
     // Register user
     const user = await User.create({

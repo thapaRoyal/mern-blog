10d7d1e4b439999019e4c21f6e7b4513ba8b7a6a
thapaRoyal <thaparoyal17@gmail.com>
2022-04-09 15:11:59 +0545

    .

--- backend/middleware/error/errorHandler.js
+++ backend/middleware/error/errorHandler.js
@@ -1,7 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   res.status(500).json({
     message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
 };
 

2455e5d0acc2a855a95d62a9489ed835d83542bf
thapaRoyal <thaparoyal17@gmail.com>
2022-04-23 15:42:11 +0545

    block user if post contains profane words

--- backend/controllers/posts/postController.js
+++ backend/controllers/posts/postController.js
@@ -1,9 +1,25 @@
 const expressAsyncHandler = require('express-async-handler');
 const Post = require('../../models/post/Post-model');
 const validateMongoId = require('../../utils/validateMongoId');
+const Filter = require('bad-words');
+const User = require('../../models/user/User-model');
 
 const createPostController = expressAsyncHandler(async (req, res) => {
+  const { _id } = req.user;
   //   validateMongoId(req.body.user);
+  //   check for bad words
+
+  const filter = new Filter();
+  const isProfane = filter.isProfane(req.body.title, req.body.description);
+  //   block user
+  if (isProfane) {
+    const user = await User.findByIdAndUpdate(_id, {
+      isBlocked: true,
+    });
+    throw new Error(
+      'You have been blocked because post contains profane words'
+    );
+  }
   try {
     const post = await Post.create(req.body);
     res.json(post);

fe2836cab017f0b33aca2bf6197da581eda22be1
thapaRoyal <thaparoyal17@gmail.com>
2022-05-13 18:20:18 +0545

    comment model

---
+++ backend/models/comment/Comment-model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema(
+  {
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post',
+      required: [true, 'Post is required'],
+    },
+    user: {
+      type: Object,
+      required: [true, 'User is required'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Description is required'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;

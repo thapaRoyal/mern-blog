bc9bb602cc16b80bc5a471a25c971dce4f5dfe6d
thapaRoyal <thaparoyal17@gmail.com>
2022-05-15 12:17:06 +0545

    register

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,14 @@ import {
   BrowserRouter,
 } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
+import Register from './components/Users/Register/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
   );

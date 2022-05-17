ba1b856c7f3e79d93ae6fa85b04be0c44beea342
thapaRoyal <thaparoyal17@gmail.com>
2022-05-17 20:25:31 +0545

    display navbar base on Login user

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import AdminNavbar from './admin/AdminNavbar';
-import PrivateNavbar from './private/PrivateNavbar';
-import PublicNavbar from './public/PublicNavbar';
+import { useSelector } from 'react-redux';
+import AdminNavbar from './Admin/AdminNavbar';
+import AccountVerificationAlertWarning from './Alerts/AccountVerificationAlertWarning';
+import AccountVerificationSuccessAlert from './Alerts/AccountVerificationSuccessAlert';
+
+import PrivateNavbar from './Private/PrivateNavbar';
+import PublicNavbar from './Public/PublicNavbar';
 
 const Navbar = () => {
+  //get user from store
+  const state = useSelector((state) => state.users);
+  const { userAuth, profile } = state;
+  const isAdmin = userAuth?.isAdmin;
+
+  //account verification
+  const account = useSelector((state) => state?.accountVerification);
+  const { loading, appErr, serverErr, token } = account;
   return (
     <>
-      <PublicNavbar />
+      {isAdmin ? (
+        <AdminNavbar isLogin={userAuth} />
+      ) : userAuth ? (
+        <PrivateNavbar isLogin={userAuth} />
+      ) : (
+        <PublicNavbar />
+      )}
+      {/* Display alert */}
+      {userAuth && !userAuth.isVerified && <AccountVerificationAlertWarning />}
+      {/* display success msg */}
+      {loading && <h2 className="text-center">Loading please wait...</h2>}
+      {token && <AccountVerificationSuccessAlert />}
+      {appErr || serverErr ? (
+        <h2 className="text-center text-red-500">
+          {serverErr} {appErr}
+        </h2>
+      ) : null}
     </>
   );
 };

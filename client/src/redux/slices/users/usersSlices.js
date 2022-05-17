71e045b8ced2beee5a5fb2ab38b46d98c1e894b4
thapaRoyal <thaparoyal17@gmail.com>
2022-05-17 09:29:03 +0545

    user login action creator

--- client/src/redux/slices/users/usersSlices.js
+++ client/src/redux/slices/users/usersSlices.js
@@ -1,22 +1,52 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { baseUrl } from '../../../utils/baseUrl';
 
 // register action
 export const registerUserAction = createAsyncThunk(
   'users/register',
   async (user, { rejectWithValue, getState, dispatch }) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
     try {
       // http call
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
       const { data } = await axios.post(
-        'http://localhost:8000/api/users/register',
+        `${baseUrl}/api/users/register`,
         user,
         config
       );
+      return data;
+    } catch (err) {
+      if (!err?.response) {
+        throw err;
+      }
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
+
+// login action
+export const loginUserAction = createAsyncThunk(
+  'user/login',
+  async (userData, { rejectWithValue, getState, dispatch }) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const { data } = await axios.post(
+        `${baseUrl}/api/users/login`,
+        userData,
+        config
+      );
+      // save user data to local storage
+      localStorage.setItem('userInfo', JSON.stringify(data));
       return data;
     } catch (err) {
       if (!err?.response) {
